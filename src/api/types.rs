b26c28206de9ae2fd538b8710c0e79fc151da3b4
Sascha Eglau <se@saschaeglau.com>
2024-02-29 15:53:09 +0800

    feat: allow specifying env variables

--- src/api/types.rs
+++ src/api/types.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter, Result};
+use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 use tabled::Tabled;
 
@@ -28,4 +30,24 @@ pub struct Service {
     pub name: String,
     pub image: String,
     pub container_port: u16,
+    pub env: DisplayHashMap
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+pub struct DisplayHashMap(HashMap<String, String>);
+
+impl Display for DisplayHashMap {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        let mut entries = self.0.iter().peekable();
+
+        while let Some((key, value)) = entries.next() {
+            write!(f, "{}: {}", key, value)?;
+
+            if entries.peek().is_some() {
+                write!(f, ", ")?;
+            }
+        }
+
+        Ok(())
+    }
 }
